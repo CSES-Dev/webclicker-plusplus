59ec8dcf7e6d493ffc38236733e83d9bf571dc63
Jerry <jerrythomasjohn9@gmail.com>
2024-12-30 17:25:36 -0800

    - Revert react version to 18 - Add ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-2xl font-semibold">WebClicker++</h1>
+        <div className="bg-primary  grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-start sm:items-center">
+                <h1 className="text-2xl text-primary-foreground font-semibold">WebClicker++</h1>
+                <Button variant="outline">Click</Button>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
                 <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    className="text-primary-foreground flex items-center gap-2 hover:underline hover:underline-offset-4"
                     href="https://csesucsd.com/"
                     target="_blank"
                     rel="noopener noreferrer"
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,16 @@
-// Example
+import { clsx } from "clsx";
+import type { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * A utility function that merges tailwind classes with conditional classes combining functionalities of twMerge and clsx.
+ *
+ * @param inputs - Array of classnames/conditional classes.
+ * @returns Merged className string.
+ */
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
 
 /**
  * A utility function that generates a greeting message.
