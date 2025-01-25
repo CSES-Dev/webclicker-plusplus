0b71ed97f232dacc5dbcea5a7adde2d7474e0c38
nselvakumar25 <nselvakumar@ucsd.edu>
2025-01-25 15:34:32 -0800

    create course list view

--- app/course-list/page.tsx
+++ app/course-list/page.tsx
@@ -1,9 +1,68 @@
 import React from "react";
 import CourseCard from "../../components/ui/CourseCard";
+import { Props } from "../../components/ui/CourseCard";
 export default function page() {
+    const placeholder: Props[] = [
+        {
+            color: "#F3AB7E",
+            days: ["Monday", "Wednesday"],
+            course: "COGS 107A: Neuroanatomy and Physiology",
+            timeStart: "10:00 am",
+            timeEnd: "10:50 am",
+        },
+        {
+            color: "#ED9D9D",
+            days: ["Monday", "Thursday"],
+            course: "COGS 107B: Neuroanatomy",
+            timeStart: "10:30 am",
+            timeEnd: "11:50 am",
+        },
+        {
+            color: "#ED9D9D",
+            days: ["Monday", "Thursday"],
+            course: "COGS 107B: Neuroanatomy",
+            timeStart: "10:30 am",
+            timeEnd: "11:50 am",
+        },
+    ];
     return (
-        <div>
-            <CourseCard/>
+        <div className="w-full flex justify-center items-center pt-6">
+            <div className="max-w-[90%]">
+                <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+                    {placeholder.map((course, idx) => {
+                        return (
+                            <CourseCard
+                                key={idx}
+                                color={course.color}
+                                days={course.days}
+                                course={course.course}
+                                timeStart={course.timeStart}
+                                timeEnd={course.timeEnd}
+                            />
+                        );
+                    })}
+
+                    {/* Add Class Card */}
+
+                    {/* mobile view */}
+                    <button className="block md:hidden flex-col w-80 h-40 bg-[#F3F3F3] border border-black rounded-xl shadow-lg">
+                        <div className="bg-[#D9D9D9] mt-4 h-4 w-full"></div>
+                        <div className="min-h-[80%] py-3 px-6 flex flex-col gap-2 items-center justify-center">
+                            <p className="text-lg text-center">Add Class +</p>
+                        </div>
+                    </button>
+
+                    {/* desktop view */}
+                    <button className="hidden md:block flex-col w-80 max-w-80 h-56 max-h-56 rounded-md shadow-lg">
+                        <div className="bg-[#18328D] min-h-[40%] max-h-[40%] w-full rounded-t-md"></div>
+                        <div className="h-[60%] max-h-[60%] bg-[#F2F5FF] w-full flex items-center justify-center">
+                            <p className="text-lg text-center font-medium text-[#18328D]">
+                                Add a Class +
+                            </p>
+                        </div>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- components/ui/CourseCard.tsx
+++ components/ui/CourseCard.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React from "react";
 
-interface Props{
-    color: string,
-    days: string[],
-    course: string,
-    timeStart: string,
-    timeEnd: string
+export interface Props {
+    color: string;
+    days: string[];
+    course: string;
+    timeStart: string;
+    timeEnd: string;
 }
-export default function CourseCard({color, days, course, timeStart, timeEnd}: Props) {
-  return (
-    <div>course-card</div>
-  )
+export default function CourseCard({ color, days, course, timeStart, timeEnd }: Props) {
+    return (
+        <>
+            {/* Mobile View */}
+            <button className="md:hidden flex flex-col w-80 h-40 border border-black rounded-xl shadow-lg">
+                <div className={`bg-[${color}] mt-4 h-4 w-full`}></div>
+
+                <div className="min-h-[60%] py-3 px-6 flex flex-col gap-2 items-start justify-start">
+                    <p className={"text-xs"}>
+                        Time: {timeStart} - {timeEnd}
+                    </p>
+                    <p className="text-xl text-left leading-tight">{course}</p>
+                </div>
+                <p className="px-6 text-xs text-[#585858]">{days.join(", ")}</p>
+            </button>
+            {/* Desktop View */}
+            <button className="hidden md:block flex-col w-80 max-w-80 h-56 max-h-56 rounded-md shadow-lg">
+                <div className={`bg-[${color}] min-h-[40%] max-h-[40%] w-full rounded-t-md `}></div>
+                <div className="h-[40%] max-h-[40%] pt-3 px-6 flex flex-col gap-2 items-start justify-start">
+                    <p className="text-xs text-[#434343]">
+                        Time: {timeStart} - {timeEnd}
+                    </p>
+                    <p className="text-lg text-left leading-tight">{course}</p>
+                </div>
+                <p className="px-6 py-4 text-xs text-left text-[#434343]">{days.join(", ")}</p>
+            </button>
+        </>
+    );
 }
