e3bfebffda51bf9d2a68b942c75e0d74ae4a7694
n1sh1thaS <nishi2919@gmail.com>
2025-02-03 21:06:58 -0800

    added colors to tailwind config

--- app/course-list/page.tsx
+++ app/course-list/page.tsx
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CourseCard from "../../components/ui/CourseCard";
 import { Props } from "../../components/ui/CourseCard";
 export default function page() {
     const placeholder: Props[] = [
         {
-            color: "#F3AB7E",
+            color: "course_pink",
             days: ["Monday", "Wednesday"],
             course: "COGS 107A: Neuroanatomy and Physiology",
             timeStart: "10:00 am",
             timeEnd: "10:50 am",
         },
         {
-            color: "#ED9D9D",
+            color: "course_yellow",
             days: ["Monday", "Thursday"],
             course: "COGS 107B: Neuroanatomy",
             timeStart: "10:30 am",
             timeEnd: "11:50 am",
         },
         {
-            color: "#ED9D9D",
+            color: "course_green",
             days: ["Monday", "Thursday"],
             course: "COGS 107B: Neuroanatomy",
             timeStart: "10:30 am",
             timeEnd: "11:50 am",
         },
     ];
+    //get courses
+    useEffect(() => {});
     return (
         <div className="w-full flex justify-center items-center pt-6">
             <div className="max-w-[90%]">
@@ -54,8 +56,8 @@ export default function page() {
 
                     {/* desktop view */}
                     <button className="hidden md:block flex-col w-80 max-w-80 h-56 max-h-56 rounded-md shadow-lg">
-                        <div className="bg-[#18328D] min-h-[40%] max-h-[40%] w-full rounded-t-md"></div>
-                        <div className="h-[60%] max-h-[60%] bg-[#F2F5FF] w-full flex items-center justify-center">
+                        <div className="bg-gray-300 min-h-[40%] max-h-[40%] w-full rounded-t-md"></div>
+                        <div className="h-[60%] max-h-[60%] bg-gray-50 w-full flex items-center justify-center">
                             <p className="text-lg text-center font-medium text-[#18328D]">
                                 Add a Class +
                             </p>
--- components/ui/CourseCard.tsx
+++ components/ui/CourseCard.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 export interface Props {
@@ -12,7 +13,10 @@ export default function CourseCard({ color, days, course, timeStart, timeEnd }: 
         <>
             {/* Mobile View */}
             <button className="md:hidden flex flex-col w-80 h-40 border border-black rounded-xl shadow-lg">
-                <div className={`bg-[${color}] mt-4 h-4 w-full`}></div>
+                <div
+                    style={{ backgroundColor: color }}
+                    className={`bg-${color} mt-4 h-4 w-full`}
+                ></div>
 
                 <div className="min-h-[60%] py-3 px-6 flex flex-col gap-2 items-start justify-start">
                     <p className={"text-xs"}>
@@ -24,7 +28,7 @@ export default function CourseCard({ color, days, course, timeStart, timeEnd }: 
             </button>
             {/* Desktop View */}
             <button className="hidden md:block flex-col w-80 max-w-80 h-56 max-h-56 rounded-md shadow-lg">
-                <div className={`bg-[${color}] min-h-[40%] max-h-[40%] w-full rounded-t-md `}></div>
+                <div className={`bg-${color} min-h-[40%] max-h-[40%] w-full rounded-t-md`}></div>
                 <div className="h-[40%] max-h-[40%] pt-3 px-6 flex flex-col gap-2 items-start justify-start">
                     <p className="text-xs text-[#434343]">
                         Time: {timeStart} - {timeEnd}
