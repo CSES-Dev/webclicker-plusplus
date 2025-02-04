201592c3ec9f2ee2ec98d49be373c0c0a61fb6ed
Ahmed Abdullahi <ahmedabdullahi083@gmail.com>
2025-02-04 06:53:23 -0800

    bar chart fix

---
+++ components/ui/bar-chartB.tsx
@@ -0,0 +1,143 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, XAxis, YAxis, LabelList, Cell } from "recharts"
+
+import { z } from 'zod';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+
+
+
+export type BarData = {
+  topic: string,
+  value: number,
+  color: string,
+  valueLeft?: number
+};
+export type ChartConfiguration = {
+  [key: string]: {
+    label: string;
+    color: string;
+  };
+};
+
+export function BarB( { title, data }: { title: string, data: BarData[] }) {
+
+  const chartData = data.map((item) => ({
+    topic: item.topic,
+    value: item.value,
+    color: item.color,
+    valueLeft: 100 - item.value
+  }));
+  const chartConfig: ChartConfiguration = chartData.reduce((acc: ChartConfiguration, item) => {
+    acc[item.topic] = {
+      label: item.topic.charAt(0).toUpperCase() + item.topic.slice(1), // Capitalize the first letter
+      color: item.color || "blue", // Map fill to color if using fill
+    };
+    return acc;
+  }, {} as ChartConfiguration);
+  
+  console.log(chartData);
+  //console.log(chartConfig);
+  
+  return (
+    <Card className="bg-transparent text-black">
+      <CardHeader className="items-center" >
+        <CardTitle className="text-black">{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="min-w-[350px] " >
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            barSize={12}
+            layout="vertical"
+            margin={{
+              left: 0,
+              right: 40,
+            }}            
+            
+          >
+            <YAxis
+              dataKey="topic"
+              type="category"
+              tickLine={false}
+              
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) =>
+                chartConfig[value as keyof typeof chartConfig]?.label
+              }
+            />
+            <XAxis dataKey="value" type="number" hide domain={[0, 100]}/>
+            {/* <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />  */}
+            <Bar 
+              dataKey="value" 
+              layout="vertical" 
+              stackId={'a'} 
+              stroke="black"
+              strokeWidth={1}
+              radius= {[25, 0, 0, 25]}
+              max={100}
+              
+            > 
+              {chartData.map((entry) => (
+                
+                  <Cell 
+                    key={entry.topic} 
+                      fill={chartConfig[entry.topic]?.color || "red"}
+                      from={0}
+                      to={entry.value}
+                       />
+                ))}
+        
+            </Bar>
+            <Bar 
+              layout="vertical" 
+              dataKey="valueLeft" 
+              fill="transparent" // Make this transparent
+              stroke="black"
+              stackId={'a'}
+              strokeWidth={1}
+              radius= {[0, 25, 25, 0]}
+            > 
+              {chartData.map((entry) => (
+                <Cell 
+                  key={`bg-${entry.topic}`} 
+                  fill="transparent" 
+                />
+              ))}
+              <LabelList
+                  dataKey="value"
+                  position="right"
+                  offset={8}
+                  formatter={(value: number) => `${value}%`}
+                  className="fill-[black]"
+                  fontSize={12}
+                />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      
+    </Card>
+  )
+}
