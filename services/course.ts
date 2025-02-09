b0e03b3c1d09d8f85eba05a1d243391d9f631372
theryanfo <ryan.s.fordham@gmail.com>
2025-02-08 20:39:42 -0800

    ran `npm run lint-fix`

--- services/course.ts
+++ services/course.ts
@@ -1,10 +1,10 @@
 "use server";
 import prisma from "@/lib/prisma";
 
-export async function getCourseWithCode(code:string) {
+export async function getCourseWithCode(code: string) {
     return await prisma.course.findFirst({
         where: {
-            code: code,
+            code,
         },
     });
 }
@@ -35,46 +35,46 @@ type AddCourseResult =
   | { error: string };
 
 export async function addCourse(
-  name: string, 
-  code: string, 
-  days: string[], 
-  color: string, 
-  startTime: string, 
-  endTime: string
+    name: string,
+    code: string,
+    days: string[],
+    color: string,
+    startTime: string,
+    endTime: string,
 ): Promise<AddCourseResult> {
-  // Check if a course with the same code already exists
-  const existingCourse = await prisma.course.findFirst({
-    where: {
-      code: code,
-    },
-  });
+    // Check if a course with the same code already exists
+    const existingCourse = await prisma.course.findFirst({
+        where: {
+            code,
+        },
+    });
 
-  if (existingCourse) {
-    return { error: "Course with this code already exists" };
-  }
+    if (existingCourse) {
+        return { error: "Course with this code already exists" };
+    }
 
-  // If no existing course, create a new one
-  const newCourse = await prisma.course.create({
-    data: {
-      title: name,
-      code: code,
-      // days: days,  // Assuming days is an array of strings
-      color: color,
-      // startTime: startTime,
-      // endTime: endTime,
-    //   users: ,
-    //   sessions: ,
-    },
-  });
+    // If no existing course, create a new one
+    const newCourse = await prisma.course.create({
+        data: {
+            title: name,
+            code,
+            // days: days,  // Assuming days is an array of strings
+            color,
+            // startTime: startTime,
+            // endTime: endTime,
+            //   users: ,
+            //   sessions: ,
+        },
+    });
 
-  return newCourse;
+    return newCourse;
 }
 
 export async function getAllCourses() {
-    try {
+  try {
       const courses = await prisma.course.findMany();
       return courses;
-    } catch (error) {
-      return { error: error };
-    }
+  } catch (error) {
+      return { error };
   }
+}
