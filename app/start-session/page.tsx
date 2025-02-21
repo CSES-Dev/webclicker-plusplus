7b45addc47b1d3b76d686a25f093000079fff7c9
janayagarcia <janayagarcia@ucsd.edu>
2025-02-20 20:40:26 -0800

    simple layout

--- app/start-session/page.tsx
+++ app/start-session/page.tsx
@@ -1,34 +1,45 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import React from 'react';
+import { Badge } from "@/components/ui/badge"
+import React, { useState } from 'react';
 
 export default function startSession() {
+    const [date, setDate] = useState("Month Day, Year");
+    const [questionType, setQuestionType] = useState("Multiple Choice");
+    const [question, setQuestion] = useState("Refer to slides.");
+    const [choices, setChoices] = useState(["A", "B", "C", "D"]);
+    const [questionNumber, setQuestionNumber] = useState(1);
+
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col justify-between items-center">
             <div className="flex justify-between w-full">
                 <Button>
                     &lt; Back
                 </Button>
-                <p>date</p>
+                <p>{date}</p>
             </div>
 
-            <div className="flex flex-col items-left w-3/4 border border-[hsl(var(--input-border))] rounded-md">
-                <p>question type</p>
-                <p>question</p>
+            <div className="flex flex-col justify-start w-3/4 border border-[hsl(var(--input-border))] rounded-md">
+                <Badge className="max-w-max">{questionType}</Badge>
+                <p>{question}</p>
             </div>
 
-            <div>
-                <p>answer progress</p>
+            <div className="flex flex-col justify-start w-3/4">
+                <p>{choices[0]}</p>
+                <p>{choices[1]}</p>
+                <p>{choices[2]}</p>
+                <p>{choices[3]}</p>
             </div>
 
-            <div>
+            <div className="flex">
                 <p>question progress</p>
             </div>
 
-            <div>
-                <p>next question</p>
+            <div className="flex justify-end w-3/4">
+                <Button>
+                    Next Question &gt;
+                </Button>
             </div>
-
         </div>
     );
-};
+}
