6525657a46b5c09a38fc914c33a096b1c609d0c2
Jerry <jerrythomasjohn9@gmail.com>
2025-03-07 11:46:59 -0800

    add useaccess hook to validate role access

---
+++ hooks/use-access.ts
@@ -0,0 +1,31 @@
+import { validateUser } from "@/services/userCourse";
+import { Role } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const useAccess = ({ courseId, role }: { courseId: number; role: Role }) => {
+    const session = useSession();
+
+    const [hasAccess, setHasAccess] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const checkAccess = async () => {
+            if (!session.data?.user.id) {
+                setHasAccess(false);
+                setIsLoading(false);
+            } else {
+                setHasAccess(
+                    (await validateUser(session.data?.user.id, courseId, "STUDENT")) ?? false,
+                );
+                setIsLoading(false);
+            }
+        };
+        void checkAccess();
+    }, [courseId, role]);
+
+    return { hasAccess, isLoading };
+};
+
+export default useAccess;
