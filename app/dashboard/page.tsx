8ff0d52a18438276c077cd078210d2caedd9b6bd
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-03-06 19:06:48 -0800

    Fixed Add Course

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Course, Role, Schedule } from "@prisma/client";
-import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { AddCourseForm } from "@/components/AddCourseForm";
@@ -64,30 +63,15 @@ export default function Page() {
                     })}
 
                     {/* Add Class Card */}
-
                     {role === "LECTURER" ? (
                         <AddCourseForm />
                     ) : (
-                        <>
-                            <Link
-                                href="/join-course"
-                                className="flex flex-col w-[476px] h-[345px] rounded-lg shadow-lg border border-gray-300"
-                            >
-                                <div className="bg-primary h-[40%] w-full rounded-t-lg"></div>
-                                <div className="h-[60%] w-full bg-gray-50 flex items-center justify-center">
-                                    <p className="text-lg font-medium text-primary">Add Class +</p>
-                                </div>
-                            </Link>
-                            <Link
-                                href="/join-course"
-                                className="hidden md:flex flex-col w-[476px] h-[345px] rounded-lg shadow-lg border border-gray-300"
-                            >
-                                <div className="bg-primary h-[40%] w-full rounded-t-lg"></div>
-                                <div className="h-[60%] w-full bg-gray-50 flex items-center justify-center">
-                                    <p className="text-lg font-medium text-primary">Add a Class +</p>
-                                </div>
-                            </Link>
-                        </>
+                        <div className="flex flex-col w-80 h-56 rounded-md shadow-lg border border-gray-300">
+                            <div className="bg-primary h-[40%] w-full rounded-t-md"></div>
+                            <div className="h-[60%] w-full bg-gray-50 flex items-center justify-center rounded-b-md">
+                                <p className="text-lg font-medium text-primary">Add Class +</p>
+                            </div>
+                        </div>
                     )}
                 </div>
             </div>
