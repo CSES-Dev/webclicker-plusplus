a2324783f3c2e871a4b53ac59d8a7b17225bfe73
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-03-08 00:15:51 -0800

    Fixing Playwrite Test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="bg-primary grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <main className="flex flex-col gap-8 row-start-2 items-start sm:items-center">
                 <h1 className="text-2xl text-primary-foreground font-semibold">
-                    WebClicker++ how you doing
+                    Welcome to WebClicker++
                 </h1>
                 <Button variant="outline">Click</Button>
             </main>
