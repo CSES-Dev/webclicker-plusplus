f8572fc9e4777581558c12017d79fb3f9d2892a7
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-03-08 01:13:19 -0800

    Playwright Test

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("Navigates to home page and checks if main text is present", async ({ page }) => {
+    await page.goto("/");
     await page.goto("/login");
     // Make sure that the 'WebClicker++' is present on screen
     await expect(page.getByText("Welcome to WebClicker++")).toBeVisible();
