dae6f2f460a96d06a487105e82c60b5dc9d1d7a3
Jerry <jerrythomasjohn9@gmail.com>
2025-06-23 18:54:32 -0700

    fix poll and remove unncessary dependencies

--- components/YAxisTick.tsx
+++ components/YAxisTick.tsx
@@ -1,6 +1,4 @@
-import { MoreHorizontal } from "lucide-react";
-import React from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { TooltipContent, StringTooltipContainer, Tooltip, TooltipTrigger } from "./ui/tooltip";
 
 interface EllipsisYAxisTickProps {
     x?: number;
@@ -11,39 +9,21 @@ interface EllipsisYAxisTickProps {
 export function LetteredYAxisTick({ x = 0, y = 0, payload }: EllipsisYAxisTickProps) {
     if (!payload) return null;
 
-    const fullText = payload.value;
-    const maxChars = 15;
-    const truncated =
-        fullText.length > maxChars ? fullText.slice(0, maxChars) + " . . . " : fullText;
 
     return (
         <g transform={`translate(${x},${y})`}>
-            <text textAnchor="end" fill="#000" dy={4} dx={-2} fontSize={12}>
-                {truncated}
-            </text>
-            {fullText.length > maxChars && (
-                <foreignObject x={-16.9} y={-6} width={20} height={20}>
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <button
-                                style={{
-                                    background: "none",
-                                    border: "none",
-                                    cursor: "pointer",
-                                    padding: 0,
-                                }}
-                            >
-                                <MoreHorizontal size={16} />
-                            </button>
-                        </PopoverTrigger>
-                        <PopoverContent>
-                            <div style={{ padding: "0.5rem 1rem", whiteSpace: "normal" }}>
-                                {fullText}
-                            </div>
-                        </PopoverContent>
-                    </Popover>
-                </foreignObject>
-            )}
+            <foreignObject x={-42} y={-8} width={48} height={24}>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <span className="text-sm inline-block w-full truncate text-black">
+                            {payload.value}
+                        </span>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <StringTooltipContainer text={payload.value} />
+                    </TooltipContent>
+                </Tooltip>
+            </foreignObject>
         </g>
     );
 }
